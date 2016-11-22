3d9df6bdd34654495218e0b53eb1cf3b3d898a23
dmatosl <danielmatos.lima@gmail.com>
2016-11-22 00:24:07 -0200

    added new cases

--- api_tests.sh
+++ api_tests.sh
@@ -34,6 +34,12 @@ echo "######## /SCHEDULE valid"
 curl -s -i -H 'Content-Type: application/json' -X POST --data '{ "docker_image": "alpine:latest", "schedule": "2016-11-18T23:59:59", "env": ["key1=value", "key2=value2"], "cmd": "sleep 120" }' $BASE_URL/schedule
 echo
 
-#echo "######## /SCHEDULE valid"
-#curl -s -i -H 'Content-Type: application/json' -X POST --data '{ "docker_image": "python:2.7", "schedule": "2016-11-17 23:59:59", "env": [ "foo=bar"] }' $BASE_URL/schedule
-#echo
+echo "######## /SCHEDULE valid eta=5min"
+run_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ" -d '+5 minutes')
+curl -s -i -H 'Content-Type: application/json' -X POST --data "{ \"docker_image\": \"alpine:latest\", \"schedule\": \"${run_date}\", \"env\": [\"key1=value\", \"key2=value2\"], \"cmd\": \"sleep 120\" }" $BASE_URL/schedule
+echo
+
+echo "######## /SCHEDULE valid eta=6min"
+run_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ" -d '+6 minutes')
+curl -s -i -H 'Content-Type: application/json' -X POST --data "{ \"docker_image\": \"python:2.7-sim\", \"schedule\": \"${run_date}\", \"env\": [\"key1=value\", \"key2=value2\"] }" $BASE_URL/schedule
+echo
